cee85a40f7769bf0acfe572efb892bf0cde1afd5
HYJ <teen14y@naver.com>
2019-06-28 10:25:12 +0900

    add last name on directory

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ function drawCard(res) {
                       <img class="card-img" src=${person.picture.large} alt="profile picture">
                   </div>
                   <div class="card-info-container">
-                      <h3 id="name" class="card-name cap">${person.name.first}</h3>
+                      <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
                       <p class="card-text">${person.email}</p>
                       <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
                   </div>
